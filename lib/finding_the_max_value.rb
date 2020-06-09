f1ac51104e6f88e38df7169cbd20c80413af4952
Francisco Andaur <francisco.andaur@icloud.com>
2020-06-09 03:20:14 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,10 +2,12 @@ require 'pry'
 
 def find_max_value(array)
   index = 0
-  max_value = array[index] > index
-  while index < array.length do
-    index += 1
+  max_value = -1
+  while count < array.length do
+    if max_value < array[index]
+      max_value = array[index]
+    end
+    count += 1
   end
-p max_value
-
+  max_value
   end
